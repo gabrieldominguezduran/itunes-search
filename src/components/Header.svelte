9f440c422b0bea8699b94c0de4625d5843525c0f
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2023-06-26 12:02:28 +0200

    Remove quiz link

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,3 @@
 <div class="flex justify-between bg-black w-full p-2 px-10 md:px-20 lg:px-52">
 	<img src="/images/apple-logo.png" alt="logo" class="h-6" />
-	<span class="text-slate-200 font-bold uppercase">Quiz</span>
 </div>
